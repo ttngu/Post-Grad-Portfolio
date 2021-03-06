784f6df0381b9fa457bfe8f16fab61c3ec3791e4
ttngu <tammyn137@gmail.com>
2021-03-06 12:35:32 -0800

    added headdata

--- src/mock/data.js
+++ src/mock/data.js
@@ -3,8 +3,8 @@ import { nanoid } from 'nanoid';
 // HEAD DATA
 export const headData = {
   title: 'Tammy Nguyen', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
